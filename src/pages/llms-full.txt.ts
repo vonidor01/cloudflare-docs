fe0ddb42306f6051a9354433522a63a3b9898218
Kian Newman-Hazel <kian@cloudflare.com>
2025-01-07 11:21:21 +0000

    [Docs Site] Improve llms-full.txt

--- src/pages/llms-full.txt.ts
+++ src/pages/llms-full.txt.ts
@@ -2,8 +2,17 @@ import type { APIRoute } from "astro";
 import { getCollection } from "astro:content";
 
 export const GET: APIRoute = async () => {
-	const markdown = await getCollection("docs")
-		.then((entries) => entries.map((entry) => entry.body))
+	const markdown = await getCollection("docs", (e) => e.body)
+		.then((entries) =>
+			entries.map((entry) => {
+				return [
+					`# ${entry.data.title}`,
+					`URL: https://developers.cloudflare.com/${entry.slug}/`,
+					`${entry.body.trim()}`,
+					"---",
+				].join("\n\n");
+			}),
+		)
 		.then((array) => array.join("\n\n"));
 
 	return new Response(markdown, {
